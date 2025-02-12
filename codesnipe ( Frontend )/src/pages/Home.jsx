887d498e8e19415e41e0e675a2a0ac0a139a1a6f
SiddharthaWork <siddharthashresthasir@gmail.com>
2025-02-12 16:48:20 +0545

    Updated Home Page

--- codesnipe ( Frontend )/src/pages/Home.jsx
+++ codesnipe ( Frontend )/src/pages/Home.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 const Home = () => {
   return (
     <div className='w-full h-full'>
-      <div className='flex justify-center items-center gap-6'>
+      <div className='flex flex-col justify-center items-center gap-6'>
         <h1 className='font-kanit text-white text-4xl'>Welcome <span className='text-sky-400'> Siddhartha</span></h1>
+        <div className='w-[20rem] h-[5rem] bg-white rounded-full p-6 flex flex-col justify-center items-start gap-2'>
+          <h1 className='font-body text-sm text-black'>Search Your Code File</h1>
+          <input type="text" className="w-full h-full outline-none border-none text-black" placeholder='Enter Your Code File Name'/>
+
+        </div>
 
       </div>
       
