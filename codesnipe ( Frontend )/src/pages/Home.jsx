6231a1ccdfe47d279d6e7cdab280a8e82948fdc0
SiddharthaWork <siddharthashresthasir@gmail.com>
2025-02-15 18:33:24 +0545

    Completed Search Design and Functionality

--- codesnipe ( Frontend )/src/pages/Home.jsx
+++ codesnipe ( Frontend )/src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Icon } from '@iconify/react/dist/iconify.js'
 import React from 'react'
 
 const Home = () => {
@@ -5,9 +6,13 @@ const Home = () => {
     <div className='w-full h-full'>
       <div className='flex flex-col justify-center items-center gap-6'>
         <h1 className='font-kanit text-white text-4xl'>Welcome <span className='text-sky-400'> Siddhartha</span></h1>
-        <div className='w-[20rem] h-[5rem] bg-white rounded-full p-6 flex flex-col justify-center items-start gap-2'>
-          <h1 className='font-body text-sm text-black'>Search Your Code File</h1>
-          <input type="text" className="w-full h-full outline-none border-none text-black" placeholder='Enter Your Code File Name'/>
+        <div className='w-[20rem] h-[2rem] bg-white rounded-full p-6 flex justify-center items-center '>
+          {/* <h1 className='font-body text-sm text-black'>Search Your Code File</h1> */}
+          
+          <div className='w-full flex gap-2 items-center justify-center'>
+            <Icon icon="mingcute:search-3-line" width="24" height="24" color='black' />
+          <input type="text" className="w-full h-full outline-none border-none text-black " placeholder='Enter Your Code File Name'/>
+          </div>
 
         </div>
 
