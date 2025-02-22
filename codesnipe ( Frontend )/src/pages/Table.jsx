249f8515127a9ea00bf604e1b109caff6562cd2d
SiddharthaWork <siddharthashresthasir@gmail.com>
2025-02-22 21:48:30 +0545

    Completed Table Components

---
+++ codesnipe ( Frontend )/src/pages/Table.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Table = () => {
+  return (
+    <div className='flex gap-6 '>
+        <div className='flex flex-col gap-2 '>
+            <h1>Here is the Demo Table Components</h1>
+        </div>
+
+    </div>
+  )
+}
+
+export default Table
