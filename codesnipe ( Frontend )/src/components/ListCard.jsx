6dda824aca5cf74abf1b7a9ee3035df97eda0005
SiddharthaWork <siddharthashresthasir@gmail.com>
2025-02-25 22:04:25 +0545

    Some changes in the List Card

--- codesnipe ( Frontend )/src/components/ListCard.jsx
+++ codesnipe ( Frontend )/src/components/ListCard.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
 const ListCard = () => {
+  let a = 10;
   return (
     <div className='w-[20rem] h-[30rem] bg-white border shadow-sm'>
         <div className='flex flex-col gap-2'>
             Here is the Demo Card
+        </div>
+        <div className='flex gap-2'>
+          Here is some List of the demo
+     
+
+
         </div>
     </div>
   )
