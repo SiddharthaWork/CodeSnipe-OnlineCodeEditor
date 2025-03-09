0a8f7bce4027add934b2244463027cb23cf26901
SiddharthaWork <siddharthashresthasir@gmail.com>
2025-03-09 18:38:12 +0545

    Some clean up

--- codesnipe ( Frontend )/src/components/GridCard.jsx
+++ codesnipe ( Frontend )/src/components/GridCard.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Avatar } from '@mui/material';
 import { Icon } from '@iconify/react';
 
 const list = [
@@ -50,39 +49,40 @@ const list = [
 const GridCard = () => {
   return (
     <div className='grid md:grid-cols-2 xl:grid-cols-3 grid-cols-1 w-full h-full gap-16 justify-center place-items-center'>
-      {list.map((item,index) => (
-         <div className='relative w-[26rem] h-[16rem] z-0 group'>
-         <div className='w-[24rem] h-[15rem] rounded-lg z-40 shadow-sm  flex flex-col gap-2'>
-          
-           <div className='w-full h-[80%]'>
-             <img src={item.img} alt="" className='w-full h-full object-cover' />
-           </div>
-           <div className='w-full h-[20%] py-2 flex gap-2 items-center justify-between'>
-             <div className='flex gap-2 items-center'>
-             <Avatar alt="Remy Sharp" sx={{ borderRadius: "0%" }}  src={item.userprofile} />
-     
-             <h1 className='font-kanit flex flex-col'>
-               <span className='text-white text-lg'>{item.title}</span>
-               <span className='text-white text-sm'>{item.des}</span>
-             </h1>
-             </div>
-     
-             <div className='w-12 h-12 rounded-full flex justify-center items-center'>
-             <Icon icon="pepicons:code" width="24" height="24" color='white' />
-             </div>
-     
-     
-           </div>
-     
-         </div>
-         <div className='absolute 
+      {list.map((item, index) => (
+        <div className='relative w-[26rem] h-[16rem] z-0 group'>
+          <div className='w-[24rem] h-[15rem] rounded-lg z-40 shadow-sm  flex flex-col gap-2'>
+
+            <div className='w-full h-[80%]'>
+              <img src={item.img} alt="" className='w-full h-full object-cover' />
+            </div>
+            <div className='w-full h-[20%] py-2 flex gap-2 items-center justify-between'>
+              <div className='flex gap-2 items-center'>
+                <div className='w-10 h-10 overflow-hidden cursor-pointer' onClick={() => showProfile(!profile)}>
+                  <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRi6rSH-A48Rg-kaRwlL7p8kFcQn6SxsxBcog&s" alt="" className='w-full h-full object-cover' />
+                </div>
+                <h1 className='font-kanit flex flex-col'>
+                  <span className='text-white text-lg'>{item.title}</span>
+                  <span className='text-white text-sm'>{item.des}</span>
+                </h1>
+              </div>
+
+              <div className='w-12 h-12 rounded-full flex justify-center items-center'>
+                <Icon icon="pepicons:code" width="24" height="24" color='white' />
+              </div>
+
+
+            </div>
+
+          </div>
+          <div className='absolute 
          w-[24rem] h-[14.5rem] group-hover:w-[26rem] group-hover:h-[18rem] group-hover:-top-3 group-hover:-left-4
          group-hover:shadow-md group-hover:shadow-sky-400 
          top-6 left-4 bg-[#050a1f]/80 -z-10 rounded-lg transition-all duration-300 ease-in-out '>
-         </div>
-         </div>
+          </div>
+        </div>
       ))}
-   
+
     </div>
   )
 }
--- codesnipe ( Frontend )/src/components/Navbar.jsx
+++ codesnipe ( Frontend )/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Avatar from '@mui/material/Avatar';
 import { useState } from 'react';
 import useOutsideClick from '../hooks/useOutsideClick';
 
