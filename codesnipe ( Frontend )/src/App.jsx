b1ec8dd97671967fa1957d4c4260778ea4dfc13d
SiddharthaWork <siddharthashresthasir@gmail.com>
2025-03-15 13:35:27 +0545

    Changes in the ui

--- codesnipe ( Frontend )/src/App.jsx
+++ codesnipe ( Frontend )/src/App.jsx
@@ -7,39 +7,16 @@ import SignUp from './pages/SignUp';
 import Login from './pages/Login';
 import Editor from './pages/Editor';
 import Code from './pages/Code';
-import { FloatingDock } from './components/Navbar';
-import { IconHome, IconUser, IconSettings } from "@tabler/icons-react"; // or any other icons
-import { FloatingNav } from './components/Topbar';
+import Navbar from './pages/Navbar';
 function App() {
-  const navigationItems = [
-    {
-      title: "Home",
-      href: "/",
-      icon: <IconHome className="w-full h-full" />,
-    },
-    {
-      title: "Profile",
-      href: "/profile",
-      icon: <IconUser className="w-full h-full" />,
-    },
-    {
-      title: "Settings",
-      href: "/settings",
-      icon: <IconSettings className="w-full h-full" />,
-    },
-  ];
+
 
   return (
     <div>
       <BrowserRouter>
-        <header className='fixed right-0 top-0 w-full h-fit z-50' >
-          {/* <Navbar /> */}
-          {/* <FloatingDock
-            items={navigationItems}
-            desktopClassName="fixed bottom-4 left-1/2 -translate-x-1/2" // Center at bottom
-            mobileClassName="fixed bottom-4 right-4" // Bottom right corner
-          /> */}
-          <FloatingNav navItems={navigationItems} />
+        <header className='fixed w-full h-fit z-50' >
+          <Navbar/>
+         
         </header>
         <main className='w-full h-full overflow-hidden mt-20'>
           <Routes>
@@ -53,7 +30,6 @@ function App() {
         </main>
 
       </BrowserRouter>
-      {/* Here are some change to be made */}
     </div>
     
   )
---
+++ codesnipe ( Frontend )/src/pages/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { FloatingDock } from '../components/Navbar';
+import { IconHome, IconUser, IconSettings } from "@tabler/icons-react"; // or any other icons
+
+
+const navigationItems = [
+    {
+      title: "Home",
+      href: "/",
+      icon: <IconHome className="w-full h-full" />,
+    },
+    {
+      title: "Profile",
+      href: "/code",
+      icon: <IconUser className="w-full h-full" />,
+    },
+    {
+      title: "Settings",
+      href: "/settings",
+      icon: <IconSettings className="w-full h-full" />,
+    },
+  ];
+const Navbar = () => {
+  return (
+    <div>
+         <FloatingDock
+            items={navigationItems}
+            desktopClassName="fixed bottom-4 left-1/2 -translate-x-1/2" // Center at bottom
+            mobileClassName="fixed bottom-4 right-4" // Bottom right corner
+          />
+    </div>
+  )
+}
+
+export default Navbar
