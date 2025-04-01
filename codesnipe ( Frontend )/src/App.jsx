2b47bdfe781586b3b72f0e458950820e3fab23a3
SiddharthaWork <siddharthashresthasir@gmail.com>
2025-04-01 12:35:42 +0545

    Changes in UI

--- codesnipe ( Frontend )/src/App.jsx
+++ codesnipe ( Frontend )/src/App.jsx
@@ -8,32 +8,41 @@ import Editor from './pages/Editor';
 import Code from './pages/Code';
 import MainNavbar from './components/MainNavbar';
 import TopNavbar from './pages/TopNavbar';
-import { useState } from 'react';
+
+// Protected Route component
+const ProtectedRoute = ({ children }) => {
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+  return isLoggedIn ? children : <Navigate to="/login" />;
+};
 
 function App() {
-  const isLoggedIn = localStorage.getItem("isLoggedIn");
-  // user authentication
   return (
-    <div>
-      <BrowserRouter>
-        <header className='w-full h-fit z-50' >
-          <MainNavbar/>
-          <TopNavbar/>
+    <BrowserRouter>
+      <div className="min-h-screen flex flex-col">
+        <header className='w-full h-fit z-50 '>
+          <MainNavbar />
+          <TopNavbar />
         </header>
-        <main className='w-full h-full overflow-hidden mt-4'>
+        
+        <main className='flex-1 w-full overflow-x-hidden mt-4'>
           <Routes> 
             <Route path='/' element={<Home />} />            
             <Route path='/signup' element={<SignUp />} />
             <Route path='/login' element={<Login />} />
-            <Route path='/code' element={isLoggedIn ? <Code /> : <Navigate to="/login" />} />
+            <Route 
+              path='/code' 
+              element={
+                <ProtectedRoute>
+                  <Code />
+                </ProtectedRoute>
+              } 
+            />
             <Route path='/editor/:projectid' element={<Editor />} />
             <Route path="*" element={<NoPage />} />
           </Routes>
         </main>
-
-      </BrowserRouter>
-    </div>
-    
+      </div>
+    </BrowserRouter>
   )
 }
 
