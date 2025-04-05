8b797c38c03704037e980442b3187c87d48f545a
SiddharthaWork <siddharthashresthasir@gmail.com>
2025-04-05 21:21:02 +0545

    Completed Some UI changes

--- codesnipe ( Frontend )/src/App.jsx
+++ codesnipe ( Frontend )/src/App.jsx
@@ -13,6 +13,7 @@ import TopNavbar from './pages/TopNavbar';
 // Create a Protected Route Improve the code reuse and optimizing
 // Create a Protected Route Componente to protect the routes and Improve the code reuse and optimizing
 // Create a Protected Route Componente to protect the routes and Improve the code optimizing and reuse
+// Some changes in the Protected Routes and the UI of the Website
 const ProtectedRoute = ({ children }) => {
   const isLoggedIn = localStorage.getItem("isLoggedIn");
   return isLoggedIn ? children : <Navigate to="/login" />;
